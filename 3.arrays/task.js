2d705073b540bc8cd07084944d3f9ae18672a8b1
Alexander.Samisko <vandermillet@gmail.com>
2021-08-16 10:54:17 +0300

    Arrays homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+ 	if (arr1.length === arr2.length){
+  result = arr1.every((item, idx) => item === arr2[idx]
+	)} else result = false
+  return result; 
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+  const positive = arr.filter((item) => item > 0);
+  const multiplicityToThree = positive.filter((item) => item % 3 === 0);
+  resultArr = multiplicityToThree.map((item) => item * 10);
 
   return resultArr; // array
 }
