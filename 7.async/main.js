ead1e5a0be68b10d9bf1c7f464a8ff20fb30454a
Alexander.Samisko <vandermillet@gmail.com>
2021-09-01 11:03:43 +0300

    Assinchronic homework v1.0

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,1 +1,18 @@
-// тут вы можете вызывать функции из task.js
+function testCase() {
+	let phoneAlarm = new AlarmClock();
+
+	phoneAlarm.addClock("10:59", () => console.log("Пора работать!"), 1);
+	phoneAlarm.addClock("11:00", () => {console.log("УВОЛЮ!!!"); phoneAlarm.removeClock(2)}, 2);
+  	try {phoneAlarm.addClock("11:01", () => console.log("Обеда не получишь!!!"));}
+  	finally {
+	phoneAlarm.addClock("11:02", () => {console.log("Обеда не получишь!!!"); 
+											phoneAlarm.clearAlarms();
+											phoneAlarm.printAlarms();
+										}, 3)
+		phoneAlarm.addClock("11:03", () => console.log("Пора работать!"), 1)
+		phoneAlarm.printAlarms();
+		phoneAlarm.start();	
+	}
+}
+
+testCase();
