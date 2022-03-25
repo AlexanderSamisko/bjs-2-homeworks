da695189227ce4049a55cd65a182254497c9e25f
AlexanderSamisko <83254509+AlexanderSamisko@users.noreply.github.com>
2022-03-25 17:30:29 +0400

    another attempt

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -5,18 +5,16 @@ function cachingDecoratorNew(func) {
   	if (key in cache) {
   		console.log(`Из кэша: ${cache[key]}`); 		
   		return `Из кэша: ${cache[key]}`
-  	} else {
-  		let result = func(...args);
-  		cache[key] = result;
-  		console.log(Object.keys(cache).length);
-  		if (Object.keys(cache).length > 5) {
-  			let keyList = Object.getOwnPropertyNames(cache);
-  			delete cache[`${keyList[0]}`];
-  		};
-  		console.log(Object.keys(cache).length);
-  		console.log(`Вычисляем: ${result}`);
-  		return `Вычисляем: ${result}`
-  	}
+  	} 
+  	let result = func(...args);
+  	cache[key] = result;
+  	if (Object.keys(cache).length > 5) {
+  		let keyList = Object.getOwnPropertyNames(cache);
+  		delete cache[`${keyList[0]}`];
+  	};
+  	console.log(`В кэше: ${Object.keys(cache).length} записей`);
+  	console.log(`Вычисляем: ${result}`);
+  	return `Вычисляем: ${result}`
   }
 }
 
@@ -24,52 +22,40 @@ function cachingDecoratorNew(func) {
 
 
 function debounceDecoratorNew(func,ms) {
-	let isSleeping = false;
-	let lastArgs;
-	
-  	function wrapper(...args) {
-  		if (isSleeping === false) {
-  			func(...args)
-  			isSleeping = true;
-  			setTimeout(() => {
-  				func(...lastArgs)
-  				isSleeping = false}
-  				,ms);
-  		} else {
-  			lastArgs = args;
-  		}
+	let isSleeping;
+	let timer;
+  	return function (...args) {
+		clearTimeout(timer);
+		timer = setTimeout(()=>{
+			func(...args);
+		},ms)
+		if (!isSleeping) {
+			func(...args);
+			isSleeping = true;
+		}
   	} 
-  	return wrapper
 }
 
 
 
 
 function debounceDecorator2(func,ms) {
-	let isSleeping = false;
-	let lastArgs;
-	let sendingTime;
-	wrapper.count = []; 
-	
-  	function wrapper(...args) {
-  				
-  		if (isSleeping === false) {
-  			sendingTime = Date.now();
-  			func(...args);
-  			wrapper.count.push(sendingTime);
-  			isSleeping = true;
-  			console.log(`Сигнал был послан ${wrapper.count.length} раз(а)`);
-  			setTimeout(() => {
-  				sendingTime = Date.now();
-  				func(...lastArgs);
-  				wrapper.count.push(sendingTime);
-  				isSleeping = false;
-  				console.log(`Сигнал был послан ${wrapper.count.length} раз(а)`)},
-  				ms)
-  		} else {
-  			lastArgs = args;
-  		}
-
+	let isSleeping;
+	let timer;
+	let count = 0;
+  	return function (...args) {
+		clearTimeout(timer);
+		timer = setTimeout(()=>{
+			func(...args);
+			count++;
+			console.log(count);
+		},ms)
+		if (!isSleeping) {
+			func(...args);
+			isSleeping = true;
+			count++
+			console.log(count);
+		}
+		
   	} 
-  	return wrapper
 }
